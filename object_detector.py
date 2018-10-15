db25cbefc86418b4ae5382174cbb8dfef0792f2c
Rajat Tyagi <34487724+bey0nd-space@users.noreply.github.com>
2018-10-15 17:00:06 +0530

    project upload

---
+++ object_detector.py
@@ -0,0 +1,16 @@
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+
+detections, objects_path = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image3.jpg"), output_image_path=os.path.join(execution_path , "image3new.jpg"), extract_detected_objects=True)
+
+
+for eachObject, eachObjectPath in zip(detections, objects_path):
+	print(eachObject["name"] + " : " + eachObject["percentage_probability"] )
+	print("Object's image saved in " + eachObjectPath)
+	print("--------------------------------")
